6f0b059928854529d17248850830570af6f1ecf0
vivat <vivatsathorn@outlook.co.th>
2024-03-28 01:00:32 +0700

    Am I alive?

--- websocket-test/server.js
+++ websocket-test/server.js
@@ -1,7 +1,19 @@
-const port = 8080
-const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({port});
+const express = require('express');
+const app = express();
+const port = 8080; // Use one port for both servers
 
+// Create an HTTP server and pass the Express app as middleware
+const server = require('http').createServer(app);
+
+// Initialize WebSocket server and attach it to the HTTP server
+const WebSocketServer = require('ws').WebSocketServer
+const wss = new WebSocketServer({server});
+
+app.get('/', (req, res) => {
+    res.send("I'm alive! No need to panic.");
+});
+
+// WebSocket connection handling
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
         console.log('Received from client: %s', message);
@@ -9,4 +21,7 @@ wss.on('connection', function (ws) {
     });
 });
 
-console.log("Websocket Server started on port " + port);
+// Start the server
+server.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
